3f83d9f7b5a4c285d011ffe6f58ed9fe05678cf2
Greg Lindahl <lindahl@pbm.com>
2020-10-28 07:54:57 -0700

    try more crypto

--- travis-ipp.bash
+++ travis-ipp.bash
@@ -1,7 +1,10 @@
 if [[ "$TRAVIS_IPP" == "TRUE" ]]; then
     sudo bash -c 'echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list'
+    sudo wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+    sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+    sudo rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
     sudo apt-get update
-    sudo apt-get install intel-ipp-2020.0-088
+    sudo apt-get install -y intel-ipp-2020.0-088
     IPPARG=""
 else
     IPPARG="--noipp"
