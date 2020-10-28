791542068962006d57752cb89ba38e5c0d4e8f03
Greg Lindahl <lindahl@pbm.com>
2020-10-28 05:57:11 -0700

    another try at travis with ipp

---
+++ travis-ipp.bash
@@ -0,0 +1,7 @@
+if [[ "$TRAVIS_IPP" == "TRUE" ]]; then
+    echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list
+    apt-get install intel-ipp-64bit
+    IPPARG=""
+else
+    IPPARG="--noipp"
+fi
