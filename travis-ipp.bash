e99c9ff598f4927350ff4024b2ccfba82ae67848
Greg Lindahl <lindahl@pbm.com>
2020-10-28 06:15:27 -0700

    sudo make me a sandwich

--- travis-ipp.bash
+++ travis-ipp.bash
@@ -1,5 +1,5 @@
 if [[ "$TRAVIS_IPP" == "TRUE" ]]; then
-    sudo echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list
+    sudo bash -c 'echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list'
     sudo apt-get install intel-ipp-64bit
     IPPARG=""
 else
