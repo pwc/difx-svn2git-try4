50915ff86ffe4e2cf6f15161f4001a11bbeae77c
Greg Lindahl <lindahl@pbm.com>
2020-10-28 07:16:46 -0700

    maybe this fixes ipp install

--- travis-ipp.bash
+++ travis-ipp.bash
@@ -1,5 +1,6 @@
 if [[ "$TRAVIS_IPP" == "TRUE" ]]; then
     sudo bash -c 'echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list'
+    sudo apt-get update
     sudo apt-get install intel-ipp-64bit-2020.0-088
     IPPARG=""
 else
